a0d3524f9646fa094188296ee25256a8dd625537
taitp <tranphuctai@gmail.com>
2021-07-15 11:17:02 +0700

    Kết thúc Bài 06: Template trong Django Part 3

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index,name="index")
+    path('list', views.viewList, name='view_list'),
+    path('', views.index,name='index')
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+from .models import Question
+
 def index(request):
     # return HttpResponse("Hello mấy cưng")
     myname = "Trần Phúc Tài"
@@ -7,3 +9,8 @@ def index(request):
     context = {"name": myname, "taisan": taisan1}
     return render(request,"polls/index.html", context)
 
+def viewList(request):
+    # list_question = get_list_or_404(Question, pk=0)
+    list_question = Question.objects.all()
+    context = {"dsquest": list_question}
+    return render(request,"polls/question_list.html", context)
