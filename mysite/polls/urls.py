a423e298c417721b727a547a8164e1c58e55383e
taitp <tranphuctai@gmail.com>
2021-07-15 14:07:23 +0700

    Kết thúc Bài 07: Select object từ database ra ngoài template

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('detail/<int:question_id>', views.detailView, name='detail'),
     path('list', views.viewList, name='view_list'),
     path('', views.index,name='index')
 ]
